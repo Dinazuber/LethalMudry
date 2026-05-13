e266b956e91b33a1b362e27060bd56843ae56c6f
SWeeX-x <noahabraao.pro@gmail.com>
2026-05-13 13:13:09 +0200

    Tiles + Map / Map Loading

--- src/lethalmudry/GameAssets.scala
+++ src/lethalmudry/GameAssets.scala
@@ -1,39 +1,36 @@
+package ch.hevs.gdx2d.lethalmudry
+
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.maps.tiled.{TiledMap, TmxMapLoader}
 import com.badlogic.gdx.utils.Disposable
 
-package lethalmudry{
+class GameAssets extends Disposable {
+  val manager: AssetManager = new AssetManager()
 
-  import com.badlogic.gdx.graphics.Texture
+  private val MAP_PATH    = "data/images/TilesBigger/maps.tmx"
+  private val PLAYER_PATH = "data/images/sprite_sheet.png"
 
+  def loadAll(): Unit = {
+    manager.setLoader(classOf[TiledMap], new TmxMapLoader(new InternalFileHandleResolver))
+    manager.load(MAP_PATH, classOf[TiledMap])
+    manager.load(PLAYER_PATH, classOf[Texture])
+  }
 
-    class GameAssets extends Disposable{
-    val manager: AssetManager = new AssetManager()
+  def updateLoading(): Boolean = {
+    manager.update()
+  }
 
-    private val MAP_PATH = "data/images/Tiles/map.tmx"
-    private val PLAYER_PATH = "data/images/lethalCompanyFull.png"
+  def getPlayerTexture(): Texture = {
+    manager.get(PLAYER_PATH, classOf[Texture])
+  }
 
-    def loadAll(): Unit = {
-      manager.setLoader(classOf[TiledMap], new TmxMapLoader(new InternalFileHandleResolver))
-      manager.load(MAP_PATH, classOf[TiledMap])
-      manager.load(PLAYER_PATH,classOf[Texture])
-    }
+  def getMap(): TiledMap = {
+    manager.get(MAP_PATH, classOf[TiledMap])
+  }
 
-    def updateLoading(): Boolean = {
-      manager.update()
-    }
-
-    def getPlayerTexture(): Texture = {
-      manager.get(PLAYER_PATH, classOf[Texture])
-    }
-
-    def getMap(): TiledMap ={
-      manager.get(MAP_PATH, classOf[TiledMap])
-    }
-
-    override def dispose(): Unit = {
-      manager.dispose()
-    }
+  override def dispose(): Unit = {
+    manager.dispose()
   }
 }
