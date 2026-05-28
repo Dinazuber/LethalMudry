dcc4f340537b29340e0e02df013aaada984d53a6
Dina <119934536+Dinazuber@users.noreply.github.com>
2026-05-28 14:20:58 +0200

    Update link to the file

--- src/lethalmudry/GameAssets.scala
+++ src/lethalmudry/GameAssets.scala
@@ -9,22 +9,22 @@ package ch.hevs.gdx2d.lethalmudry {
   class GameAssets extends Disposable {
     val manager: AssetManager = new AssetManager()
 
-  private val MAP_PATH    = "data/images/TilesBigger/maps.tmx"
-  private val PLAYER_PATH = "data/images/sprite_sheet.png"
-  private val BATTERY_PATH = "data/images/battery.png"
-  private val HEAL_PATH = "data/images/heal.png"
-  private val BOLT_PATH = "data/images/scrap/big_bolt.png"
-  private val WATER_PATH = "data/images/scrap/water.png"
+    private val MAP_PATH = "data/images/TilesBigger/map.tmx"
+    private val PLAYER_PATH = "data/images/sprite_sheet.png"
+    private val BATTERY_PATH = "data/images/battery.png"
+    private val HEAL_PATH = "data/images/heal.png"
+    private val BOLT_PATH = "data/images/scrap/big_bolt.png"
+    private val WATER_PATH = "data/images/scrap/water.png"
 
-  def loadAll(): Unit = {
-    manager.setLoader(classOf[TiledMap], new TmxMapLoader(new InternalFileHandleResolver))
-    manager.load(MAP_PATH, classOf[TiledMap])
-    manager.load(PLAYER_PATH, classOf[Texture])
-    manager.load(BATTERY_PATH, classOf[Texture])
-    manager.load(HEAL_PATH, classOf[Texture])
-    manager.load(BOLT_PATH, classOf[Texture])
-    manager.load(WATER_PATH, classOf[Texture])
-  }
+    def loadAll(): Unit = {
+      manager.setLoader(classOf[TiledMap], new TmxMapLoader(new InternalFileHandleResolver))
+      manager.load(MAP_PATH, classOf[TiledMap])
+      manager.load(PLAYER_PATH, classOf[Texture])
+      manager.load(BATTERY_PATH, classOf[Texture])
+      manager.load(HEAL_PATH, classOf[Texture])
+      manager.load(BOLT_PATH, classOf[Texture])
+      manager.load(WATER_PATH, classOf[Texture])
+    }
 
     def updateLoading(): Boolean = {
       manager.update()
@@ -38,23 +38,24 @@ package ch.hevs.gdx2d.lethalmudry {
       manager.get(MAP_PATH, classOf[TiledMap])
     }
 
-  def getBatteryTexture() : Texture = {
-    manager.get(BATTERY_PATH, classOf[Texture])
-  }
+    def getBatteryTexture(): Texture = {
+      manager.get(BATTERY_PATH, classOf[Texture])
+    }
 
-  def getHealTexture(): Texture = {
-    manager.get(HEAL_PATH, classOf[Texture])
-  }
+    def getHealTexture(): Texture = {
+      manager.get(HEAL_PATH, classOf[Texture])
+    }
 
-  def getBoltTexture(): Texture = {
-    manager.get(BOLT_PATH, classOf[Texture])
-  }
+    def getBoltTexture(): Texture = {
+      manager.get(BOLT_PATH, classOf[Texture])
+    }
 
-  def getWaterTexture(): Texture = {
-    manager.get(WATER_PATH, classOf[Texture])
-  }
+    def getWaterTexture(): Texture = {
+      manager.get(WATER_PATH, classOf[Texture])
+    }
 
-  override def dispose(): Unit = {
-    manager.dispose()
+    override def dispose(): Unit = {
+      manager.dispose()
+    }
   }
 }
