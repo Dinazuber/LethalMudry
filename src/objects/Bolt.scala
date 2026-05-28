a367e601712a8d311ccf547031c6b09b809ae43e
Dina <119934536+Dinazuber@users.noreply.github.com>
2026-05-28 13:18:50 +0200

    Fixing the inventory bar changes after collecting a bolt

--- src/objects/Bolt.scala
+++ src/objects/Bolt.scala
@@ -5,8 +5,10 @@ import com.badlogic.gdx.graphics.Texture
 
 class Bolt(x: Float, y: Float, texture: Texture, width: Float, height: Float) extends Object(x, y, width, height, texture){
   override def collect(player: Player, game: LethalMudry): Unit = {
-    val currentInventory = game.healthBar.getValue
-    game.inventoryBar.setValue(currentInventory + 0.5f)
+    val currentInventory = game.inventoryBar.getValue
+    println(s"the user has : ${currentInventory}")
+    game.inventoryBar.setValue(currentInventory + 10f)
+    println(s"new stock is : ${game.inventoryBar.getValue}")
     println("Bolt collected")
   }
 }
