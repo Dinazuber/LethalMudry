d44652ff7c99f09fb58002c86cca501a18482c0c
Dina <119934536+Dinazuber@users.noreply.github.com>
2026-06-01 18:33:50 +0200

    Adding death screen when health bar is 0

--- src/lethalmudry/ui/menu/DeathScreen.scala
+++ src/lethalmudry/ui/menu/DeathScreen.scala
@@ -6,19 +6,24 @@ import ch.hevs.gdx2d.lib.GdxGraphics
 import ch.hevs.gdx2d.lib.physics.PhysicsWorld
 import com.badlogic.gdx.graphics.{Color}
 
-class MenuScreen extends RenderingScreen {
+class DeathScreen extends RenderingScreen {
 
   protected var imgBackground: BitmapImage = _
 
   override def onInit(): Unit = {
-    imgBackground = new BitmapImage("data/images/backgroundMenu.png")
+    imgBackground = new BitmapImage("data/images/GameOver.png")
   }
 
   override def onGraphicRender(g: GdxGraphics): Unit = {
+    // Reset the camera to the center of the screen
+    val camera = g.getCamera
+    camera.position.set(g.getScreenWidth / 2f, g.getScreenHeight / 2f, 0)
+    camera.zoom = 1f
+    camera.update()
+
     g.clear(Color.BLACK)
-    g.drawPicture((g.getScreenWidth / 4).toFloat, (g.getScreenHeight / 2).toFloat, imgBackground)
-    g.drawPicture((g.getScreenWidth / 1.2).toFloat, (g.getScreenHeight / 2).toFloat, imgBackground)
-    g.drawStringCentered((g.getScreenHeight / 2).toFloat, "Welcome to LethalMudry")
+    g.setColor(Color.WHITE)
+    g.drawPicture(g.getScreenWidth / 2, g.getScreenHeight / 2, imgBackground)
     g.drawStringCentered((g.getScreenHeight / 3).toFloat, "Press Enter to play")
   }
 
